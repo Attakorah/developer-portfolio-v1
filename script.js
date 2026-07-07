cce94b7cb99483fc5ed09d8c79f80e6f4e756c1e
Attakorah <attakorah2005@gmail.com>
2026-07-07 15:32:45 +0000

    Add dynamic footer year

---
+++ script.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+
+const footer = document.querySelector("footer p");
+
+footer.textContent = `© ${currentYear} Attakorah Emmanuel. All rights reserved.`;
