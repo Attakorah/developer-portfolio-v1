e49fbe9fd99707f64d672732a367193816f5f417
Attakorah <attakorah2005@gmail.com>
2026-07-07 15:47:01 +0000

    Add project filtering

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const currentYear = new Date().getFullYear();
 const footer = document.querySelector("footer p");
 
 footer.textContent = `© ${currentYear} Attakorah Emmanuel. All rights reserved.`;
+
+const filterButtons = document.querySelectorAll(".filter-buttons button");
+const projectCards = document.querySelectorAll("#projects article");
+
+filterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const selectedFilter = button.dataset.filter;
+
+        projectCards.forEach(project => {
+            const projectCategory = project.dataset.category;
+
+            if (selectedFilter === "all" || selectedFilter === projectCategory) {
+                project.style.display = "block";
+            } else {
+                project.style.display = "none";
+            }
+        });
+    });
+});
